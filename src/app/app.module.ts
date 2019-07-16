144f0b0e48d534192720010c12b720c1beca95a4
Unknown <diego.ignacio.mayorga@gmail.com>
2019-07-16 02:54:38 -0400

    Home Componete
    Se crea Home Component con su vista y css. Ademas se agrega esta al routing para que sea la pagina principal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],

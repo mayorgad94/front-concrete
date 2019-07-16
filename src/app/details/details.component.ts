5b1f7ccc77c1bc969de52efcf9d1b30b43aad492
Unknown <diego.ignacio.mayorga@gmail.com>
2019-07-16 03:10:23 -0400

    Details, notfound, Home , Navbar - Functions
    Se agregan funciones a los componentes anteriormente mencionados.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GitHubService } from '../API/gitHubApi/git-hub.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private apiService: GitHubService,private router: Router) { }
+
+  public nameGit;
+  public user;
+  public repositories;
+  public load = true;
+  public notData = false;
+  public order: string = 'stargazers_count';
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.nameGit = params.get('nameGit');
+      this.getUserGit(this.nameGit);
+    });
+  }
+
+  getUserGit(name){
+    this.apiService.getUser(name).subscribe((res :any)=>{
+        console.log(res)
+        this.user = res;
+        this.load = false
+        this.getRepositories(name);
+    }, error => {
+      this.router.navigate(['/notFound']);
+    });  
+  }
+  getRepositories(name){
+    this.apiService.getUserDetail(this.nameGit).subscribe((res)=>{
+      console.log(res)
+        this.repositories = res;
+    }); 
   }
 
 }

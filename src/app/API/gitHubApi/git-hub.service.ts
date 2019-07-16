9855865d9d150877a50f3dd3aaf66782b40b634e
Unknown <diego.ignacio.mayorga@gmail.com>
2019-07-16 03:05:17 -0400

    Creacion HHTP REQUEST
    Se realiza la codificacion de los servicios para consumir funcionalidades de github para obenter data de los repositorios

--- src/app/API/gitHubApi/git-hub.service.ts
+++ src/app/API/gitHubApi/git-hub.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url = 'https://api.github.com/';
+
+  getUser(nameUser){
+    return this.http.get(this.url+'users/'+nameUser);
+  }
+  getUserDetail(nameUser){
+    return this.http.get(this.url+'users/'+nameUser+'/repos');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule }   from '@angular/forms';
 import { DetailsComponent } from './details/details.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { GitHubService } from './API/gitHubApi/git-hub.service';
 
 @NgModule({
   declarations: [
@@ -19,10 +21,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GitHubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
